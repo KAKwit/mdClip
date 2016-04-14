426193e3086c85a96b9156e497ff1c1f3f3a4f45
Karl Remin <karl.remin@gmail.com>
2016-04-14 19:27:40 +1000

    Neaten, fancy, and work
    Trying to neaten the javascript a bit more, trying to make it look more
    reasonable, and trying to making it work!  Heading level and whether to
    include icon should be working now and storing settings.

--- src/mdClip.js
+++ src/mdClip.js
@@ -1,115 +1,174 @@
-var contentContainerSelector  = 'content';
-var optionsContainerSelector  = 'options';
-var copyAgainButtonSelector   = 'copy-again';
-var optionsButtonSelector     = 'options-button';
+mdClip = function() {
 
-function getTabDetails(callback) {
+	/* Document object selector names */
+	var selectors = {
+		contentContainer  : 'content',
+		optionsContainer  : 'options',
+		copyAgainButton   : 'copy-again',
+		optionsButton     : 'options-button',
+		headingLevel      : 'heading-level',
+		headingLevelValue : 'heading-level-value',
+		includeIconFalse  : 'include-icon-false',
+		includeIconTrue   : 'include-icon-true'
+	};
+	
+	/* Messages sent to the window */
+	var messageNames = {
+		getDescription : 'get-description',
+		getSelection   : 'get-selection'
+	}
+	
+	/* Parameters for querying current tab */
 	var queryInfo = {
 		active: true,
 		currentWindow: true
 	};
-	
-	chrome.tabs.query(queryInfo, function(tabs) {
-		var details = {
-			url: '',
-			title: '',
-			description: '',
-			selection: '',
-			iconUrl: ''
-		};
 
-		var tab = tabs[0];
-		details.url = tab.url;
-		details.title = tab.title;
-		details.iconUrl = tab.favIconUrl;
-		
-		chrome.tabs.sendMessage(tab.id, {message: 'get-description'}, function(response) {
-			details.description = response;
-			chrome.tabs.sendMessage(tab.id, {message: 'get-selection'}, function(response) {
-				details.selection = response;
-				callback(details);
+	/* Details to include in a clip */
+	var details = {
+		url: '',
+		title: '',
+		description: '',
+		selection: '',
+		iconUrl: ''
+	};
+
+	/* Settings and options */
+	var settings = {
+		headingLevel: 3,
+		includeIcon: true,
+		copyToClipboard: true,
+		theme: 'default'
+	};
+
+	/* Gather together the details, create the clip, and copy it to clipboard */
+	function getAndRenderClip() {
+		getTabDetails(function(details) {
+			getSettings(function(settings) {
+				document.getElementById(selectors.contentContainer).innerHTML = createClip(details, settings);
+				if (settings.copyToClipboard) {
+					copyToClipboard();
+				}
 			});
 		});
-	});
-}
-
-function getSettings(callback) {
-	chrome.storage.local.get(['settings'], function(storage) {
-		var settings = {
-			headingLevel: 3,
-			includeIcon: true,
-			copyToClipboard: true,
-			theme: 'default'
-		};
-		if (storage) {
-			for (var key in storage) {
-				settings[key] = storage[key];
-			}
-		}
-		document.getElementById('headingLevel').value = settings.headingLevel;
-		callback(settings);
-	});	
-}
-
-function putSettings(settings, callback) {
-	chrome.storage.local.set({settings: settings}, function() {
-		callback();
-	});
-}
-
-function createClip(details, settings) {
-	var para = '<br/><br/>'
-	var clip = Array(settings.headingLevel + 1).join("#") + ' ' + details.title + para;
-
-	if (details.iconUrl && settings.includeIcon) {
-		clip += '![icon](' + details.iconUrl + ')' + para;
 	}
 
-	if (details.description && details.description !== '') {
-		clip += details.description + para;
-	}
-
-	if (details.selection && details.selection !== '') {
-		clip += '> ' + details.selection + para;
-	}
-
-	clip += details.url + '<br/>';
-	return clip;
-}
-
-function copyToClipboard() {
-	var el = document.getElementById(contentContainerSelector);
-	el.focus();
-	document.execCommand('SelectAll');
-    document.execCommand('copy');
-	window.getSelection().empty();
-}
-
-function getAndRenderClip() {
-	getTabDetails(function(details) {
-		getSettings(function(settings) {
-			document.getElementById(contentContainerSelector).innerHTML = createClip(details, settings);
-			if (settings.copyToClipboard) {
-				copyToClipboard();
-			}
+	/* Get the details for the clip from both the Chrome tab and window details */
+	function getTabDetails(callback) {
+		chrome.tabs.query(queryInfo, function(tabs) {
+			var tab = tabs[0];
+			details.url = tab.url;
+			details.title = tab.title;
+			details.iconUrl = tab.favIconUrl;
+			chrome.tabs.sendMessage(tab.id, {message: messageNames.getDescription}, function(response) {
+				details.description = response;
+				chrome.tabs.sendMessage(tab.id, {message: messageNames.getSelection}, function(response) {
+					details.selection = response;
+					callback(details);
+				});
+			});
 		});
-	});
-}
-
-function showHideOptions() {
-	var content = document.getElementById(contentContainerSelector);
-	var optionsButton = document.getElementById(optionsButtonSelector);
-	var copyButton = document.getElementById(copyAgainButtonSelector);
+	}
 	
-	var showOptions = (content.className === 'in');
-	showOptions ? content.className = '' : content.className = 'in';
-	showOptions ? copyButton.className = '' : copyButton.className = 'in';
-	showOptions ? options.className = 'in' : options.className = '';
-	showOptions ? optionsButton.value = 'Okay' : optionsButton.value = 'Settings';
-}
+	/* Build a markdown clip from current details as per settings */
+	function createClip(details, settings) {
+		var br = '<br/><br/>'
+		var clip = Array(settings.headingLevel + 1).join("#") + ' ' + details.title + br;
+
+		if (details.iconUrl && settings.includeIcon) {
+			clip += '![icon](' + details.iconUrl + ')' + br;
+		}
+
+		if (details.description && details.description !== '') {
+			clip += details.description + br;
+		}
+
+		if (details.selection && details.selection !== '') {
+			clip += '> ' + details.selection + br;
+		}
+
+		clip += details.url + '<br/>';
+		return clip;
+	}
+	
+	/* Copy the current content to the clipboard */
+	function copyToClipboard() {
+		var el = document.getElementById(selectors.contentContainer);
+		el.focus();
+		document.execCommand('SelectAll');
+		document.execCommand('copy');
+		window.getSelection().empty();
+	}
+
+	/* Retrieve the current settings from storage and merge with defaults */
+	function getSettings(callback) {
+		chrome.storage.local.get(['settings'], function(storage) {
+			if (storage && storage['settings']) {
+				storage = storage['settings']
+				for (var key in storage) {
+					settings[key] = storage[key];
+				}
+			}
+			console.log(settings);
+			document.getElementById(selectors.headingLevel).value = settings.headingLevel;
+			document.getElementById(selectors.headingLevelValue).textContent = settings.headingLevel;
+			settings.includeIcon ? document.getElementById(selectors.includeIconTrue).setAttribute('checked', true) : document.getElementById(selectors.includeIconTrue).removeAttribute('checked');
+			settings.includeIcon ? document.getElementById(selectors.includeIconFalse).removeAttribute('checked') : document.getElementById(selectors.includeIconFalse).setAttribute('checked', true);
+			callback(settings);
+		});	
+	}
+	
+	/* Put setting back to storage */
+	function putSettings(callback) {
+		chrome.storage.local.set({settings: settings}, function() {
+			callback();
+		});
+	}
+
+	/* Show or hide the options window */
+	function showHideOptions() {
+		var content = document.getElementById(selectors.contentContainer);
+		var optionsButton = document.getElementById(selectors.optionsButton);
+		var copyButton = document.getElementById(selectors.copyAgainButton);
+		showOptions = (content.className === 'in');
+		showOptions ? content.className = '' : content.className = 'in';
+		showOptions ? copyButton.className = '' : copyButton.className = 'in';
+		showOptions ? options.className = 'in' : options.className = '';
+		showOptions ? optionsButton.value = 'Okay' : optionsButton.value = 'Settings';
+		
+		if (!showOptions) {
+			updateSettingsFromScreen();
+		}
+	}
+
+	/* Gather the settings back from the screen and store */
+	function updateSettingsFromScreen() {
+		settings.headingLevel = parseInt(document.getElementById(selectors.headingLevel).value);
+		settings.includeIcon = (document.querySelector('input[name=include-icon]:checked').value === 'true');
+		console.log(settings.headingLevel);
+		console.log(settings.includeIcon);
+		putSettings(function() {
+			getAndRenderClip();
+		});
+	}
+	
+	/* Update the level value next to the range slider */
+	function updateLevelValue() {
+		var level = document.getElementById(selectors.headingLevel).value;
+		document.getElementById(selectors.headingLevelValue).textContent = level;
+	}
+	
+	return {
+		getAndRenderClip: getAndRenderClip,
+		copyToClipboard: copyToClipboard,
+		showHideOptions: showHideOptions,
+		updateLevelValue: updateLevelValue
+	};
+}();
 
 document.addEventListener('DOMContentLoaded', function() {
-	document.getElementById(copyAgainButtonSelector).addEventListener('click', copyToClipboard);
-	document.getElementById(optionsButtonSelector).addEventListener('click', showHideOptions);
-	getAndRenderClip();
+	document.getElementById('copy-again').addEventListener('click', mdClip.copyToClipboard);
+	document.getElementById('options-button').addEventListener('click', mdClip.showHideOptions);
+	document.getElementById('heading-level').addEventListener('input', mdClip.updateLevelValue);
+	mdClip.getAndRenderClip();
 });
